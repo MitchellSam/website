0ad2da5e910b98c1785071febf8714e0e3efa19c
Mitchell Sam <msam478@gmail.com>
2019-05-14 11:58:50 -0400

    server-working

--- server/index.js
+++ server/index.js
@@ -1,34 +1,47 @@
-'use strict';
-
-const Hapi = require('@hapi/hapi');
 const Path = require('path');
+const Hapi = require('@hapi/hapi');
+const Inert = require('@hapi/inert');
+const Vision = require('@hapi/vision');
 
-const start = async () => {
+const publicPath = Path.join(__dirname, '../public');
 
-    const server = Hapi.server({
-        port: 8080,
-        host: 'localhost',
-        routes: {
-            files: {
-                relativeTo: Path.join(__dirname, 'public')
-            }
-        }
-    });
+const server = new Hapi.Server({
+    port: 8080,
+    routes: {
+        files: {
+            relativeTo: publicPath,
+        },
+    },
+});
 
-    await server.register(require('@hapi/inert'));
+const provision = async () => {
+    await server.register(Inert);
+    await server.register(Vision);
 
     server.route({
         method: 'GET',
-        path: '/index.html',
-        handler: function (request, h) {
-
-            return h.file('index.html');
-        }
+        path: '/images/{param*}',
+        handler: {
+            directory: {
+                path: Path.join(publicPath, 'images'),
+            },
+        },
     });
 
-    await server.start();
+    server.route({
+        method: 'GET',
+        path: '/{param*}',
+        handler: {
+            file: Path.join(publicPath, '/index.html'),
+        },
+    });
 
+    try {
+        await server.start();
+    } catch (err) {
+        console.log(err);
+    }
     console.log('Server running at:', server.info.uri);
 };
 
-start();
+provision();
