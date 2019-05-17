9d47ae79a7c4dbdc0772f0614ced246a702f2afb
Mitchell Sam <msam478@gmail.com>
2019-05-17 17:32:22 -0400

    create-post-working

--- server/routes/posts.js
+++ server/routes/posts.js
@@ -4,44 +4,61 @@ module.exports = [{
     method: 'GET',
     path: '/api/posts',
     handler: async function (request, h) {
-        const data = await db.query('SELECT * FROM posts')
-        return h.response(data.rows)
+        try {
+            const data = await db.query('SELECT * FROM posts')
+            return h.response(data.rows)
+        } catch (error) {
+            console.error(error)
+        }
     }
 }, {
     method: 'GET',
     path: '/api/posts/{id}',
     handler: async function (request, h) {
-        const data = await db.query('SELECT * FROM posts WHERE id = $1', [request.params.id])
-        return h.response(data.rows)
+        try {
+            const data = await db.query('SELECT * FROM posts WHERE id = $1', [request.params.id])
+            return h.response(data.rows)
+        } catch (error) {
+            console.error(error)
+        }
     }
 }, {
     method: 'POST',
     path: '/api/posts',
-    handler: async function (request, h) {
-        let payload = request.payload
-        payload = {
-            title: payload.title, 
-            content: payload.content, 
-            userId: payload.userId || 1
+    handler: async function (request, h) {        
+        try {
+            let payload = request.payload
+            payload = {
+                title: payload.title, 
+                content: payload.content, 
+                userId: payload.userId || 1
+            }
+            const data = await db.query(`INSERT INTO posts (title, content, "userId") VALUES ($1, $2, $3)`, [payload.title, payload.content, payload.userId])
+            return h.response(data.rows)
+        } catch (error) {
+            console.error(error)
         }
-        console.log('request', payload)
-        // const data = await db.query(`INSERT INTO posts DEFAULT VALUES;`)
-        const data = await db.query(`INSERT INTO posts (title, content, userId) VALUES ($1, $2, $3)`, [payload.title, payload.content, payload.userId])
-        console.log('data', data.rows)
-        return h.response(data.rows)
     }
 }, {
     method: 'PUT',
     path: '/api/posts/{id}',
     handler: async function (request, h) {
-        const data = await db.query('SELECT * FROM posts WHERE id = $1', [request.params.id])
-        return h.response(data.rows)
+        try {
+            const data = await db.query('SELECT * FROM posts WHERE id = $1', [request.params.id])
+            return h.response(data.rows)
+        } catch (error) {
+            console.error(error)
+        }
     }
 }, {
     method: 'DELETE',
     path: '/api/posts/{id}',
     handler: async function (request, h) {
-        const data = await db.query('DELETE FROM posts WHERE id = $1', [request.params.id])
-        return h.response(data.rows)
+        try {
+            const data = await db.query('DELETE FROM posts WHERE id = $1', [request.params.id])
+            return h.response(data.rows)
+        } catch (error) {
+            console.error(error)
+        }
     }
 }];
